57d50311d47c8f35bd40762ca9a20e913d6e06b2
val martinchik <valmartsin@gmail.com>
2018-08-03 21:47:38 +0300

    LWDEV-8703 changing month to day in the prepaired report logic

--- src/MarginTrading.NotificationGenerator/Startup.cs
+++ src/MarginTrading.NotificationGenerator/Startup.cs
@@ -136,11 +136,7 @@ namespace MarginTrading.NotificationGenerator
                 var registry = new Registry();
                 var settingsCalcTime = ApplicationContainer.Resolve<NotificationGeneratorSettings>()
                     .MonthlyTradingReportSettings.InvocationTime;
-                
-                //TODO ToRunEvery(0) FOR TEST PURPOSES
-                registry.Schedule<MonthlyTradingReportJob>().ToRunEvery(1).Months().On(1)
-                    .At(settingsCalcTime.Hours, settingsCalcTime.Minutes);
-
+           
                 registry.Schedule<DailyTradingReportJob>().ToRunEvery(1).Days()
                     .At(settingsCalcTime.Hours, settingsCalcTime.Minutes);
 
